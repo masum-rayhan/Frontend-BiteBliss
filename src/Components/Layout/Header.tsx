22d0f9d3b6ae63d94542da5005c03367731bdb4c
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-19 12:16:44 +0600

    init

---
+++ src/Components/Layout/Header.tsx
@@ -0,0 +1,176 @@
+import { useEffect, useState } from "react";
+import logo from "../../assets/mango.png";
+
+const Header = () => {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://foodfancywebapi.azurewebsites.net/menuItem")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+
+  //   const shoppingCartFromStore: cartItemModel[] = useSelector(
+  //     (state: RootState) => state.shoppingCartStore.cartItems ?? []
+  //   );
+
+  //   const userData: userModel = useSelector(
+  //     (state: RootState) => state.userAuthStore
+  //   );
+
+  //   const handleLogOut = () => {
+  //     localStorage.removeItem("token");
+  //     dispatch(setLoggedInUser({...emptyUserState}));
+  //     navigate("/");
+  //   };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          {/* <NavLink className="nav-link" aria-current="page" to="/">
+            <img src={logo} style={{ height: "40px" }} className="m-1" alt="" />
+          </NavLink> */}
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
+              <li className="nav-item">
+                {/* <NavLink className="nav-link" aria-current="page" to="/">
+                  Home
+                </NavLink> */}
+              </li>
+              <li className="nav-item">
+                {/* <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/shopping-cart"
+                >
+                  <i className="bi bi-cart"></i>{" "}
+                  {userData.nameid && `(${shoppingCartFromStore.length})`}
+                </NavLink> */}
+              </li>
+              {/* <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/authentication"
+                >
+                  Authentication
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/authorization"
+                >
+                  Authorization
+                </NavLink>
+              </li> */}
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Admin Panel
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <div className="d-flex" style={{ marginLeft: "auto" }}>
+                {/* {userData.nameid && (
+                  <>
+                    <li className="nav-item">
+                      <button
+                        className="nav-link active"
+                        style={{
+                          cursor: "pointer",
+                          background: "transparent",
+                          border: "none",
+                        }}
+                      >
+                        Welcome, {userData.unique_name}
+                      </button>
+                    </li>
+                    <li className="nav-item">
+                      <button
+                        className="btn btn-success btn-outlined rounded-pill text-white mx-2"
+                        style={{
+                          border: "none",
+                          height: "40px",
+                          width: "100px",
+                        }}
+                        onClick={handleLogOut}
+                      >
+                        Logout
+                      </button>
+                    </li>
+                  </>
+                )}
+
+                {!userData.nameid && (
+                  <>
+                    <li className="nav-item text-white">
+                      <NavLink className="nav-link" to="/register">
+                        Register
+                      </NavLink>
+                    </li>
+                    <li className="nav-item text-white">
+                      <NavLink
+                        className="btn btn-success btn-outlined rounded-pill text-white mx-2"
+                        style={{
+                          border: "none",
+                          height: "40px",
+                          width: "100px",
+                        }}
+                        to="/login"
+                      >
+                        Login
+                      </NavLink>
+                    </li>
+                  </>
+                )} */}
+              </div>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;

c5c2d8be5af972e4356b7516fdec4e5f7644c714
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-20 07:35:58 +0600

    feat: Add cart item model and shopping cart model

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,5 +1,7 @@
 import menuItemModel from "./menuItemModel";
 import userModel from "./userModel";
 import apiResponse from "./apiResponse";
+import cartItemModel from "./cartItemModel";
+import shoppingCartModel from "./shoppingCartModel";
 
-export type { menuItemModel, userModel, apiResponse };
+export type { menuItemModel, userModel, apiResponse, cartItemModel, shoppingCartModel };
